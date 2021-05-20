a115f1c19e67b5a737fc8d82e64b7dc11b7ea4ae
csbabcock <52185272+csbabcock@users.noreply.github.com>
2021-05-20 14:23:04 -0400

    Edit

---
+++ build/precache-manifest.f45b74e8cee22bcf145a1a6dad71a7a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f89fb2ab0e5eb9b8f6d73d2f832c15e9",
+    "url": "/csbabcock.com/index.html"
+  },
+  {
+    "revision": "d8a8b7ee6694e43169d6",
+    "url": "/csbabcock.com/static/css/main.bdae5be4.chunk.css"
+  },
+  {
+    "revision": "46269ad10c90738c0dd6",
+    "url": "/csbabcock.com/static/js/2.9e5dc65e.chunk.js"
+  },
+  {
+    "revision": "d8a8b7ee6694e43169d6",
+    "url": "/csbabcock.com/static/js/main.7579d77b.chunk.js"
+  },
+  {
+    "revision": "f494f00521842379aac5",
+    "url": "/csbabcock.com/static/js/runtime-main.91dd5049.js"
+  }
+]);
