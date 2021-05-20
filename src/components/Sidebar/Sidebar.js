7f431f734721942e14a708f8bebaec028f4d3bc3
csbabcock <52185272+csbabcock@users.noreply.github.com>
2021-05-20 10:33:40 -0400

    Portfolio

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,61 +1,37 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import { Location } from '@reach/router'
-import {
-  SideNavItems,
-  SideNavLink,
-} from 'carbon-components-react/lib/components/UIShell'
-import { ArrowRight16 } from '@carbon/icons-react'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { SideNavItems, SideNavLink } from 'carbon-components-react/lib/components/UIShell';
 
-import { StyledSideNav, ViewResumeButton } from './styles'
+import { StyledSideNav } from './styles';
 
 const items = [
   { name: 'Me', path: '/' },
   { name: 'Projects', path: '/projects' },
   { name: 'Work', path: '/work' },
   { name: 'Education', path: '/education' },
-]
+];
 
 const Sidebar = () => {
+  const location = useLocation();
+
   return (
-    <StyledSideNav
-      isFixedNav
-      expanded
-      isChildOfHeader={false}
-      aria-label="Side navigation"
-    >
+    <StyledSideNav isFixedNav expanded isChildOfHeader={false} aria-label="Side navigation">
       <SideNavItems>
-        <Location>
-          {({ location }) => {
-            return (
-              <>
-                {items.map(i => (
-                  <SideNavLink
-                    isActive={
-                      location.pathname === '/' && i.path === '/'
-                        ? true
-                        : location.pathname.slice(1).includes(i.path)
-                    }
-                    element={Link}
-                    to={i.path}
-                    key={i.name}
-                  >
-                    {i.name}
-                  </SideNavLink>
-                ))}
-              </>
-            )
-          }}
-        </Location>
-        <div>
-          <ViewResumeButton href="https://gitconnected.com/csbabcock">
-            <span>View Résumé</span>
-            <ArrowRight16 />
-          </ViewResumeButton>
-        </div>
+        {items.map(i => (
+          <SideNavLink
+            isActive={
+              location.pathname === '/' && i.path === '/' ? true : location.pathname === i.path
+            }
+            element={Link}
+            to={i.path}
+            key={i.name}
+          >
+            {i.name}
+          </SideNavLink>
+        ))}
       </SideNavItems>
     </StyledSideNav>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
