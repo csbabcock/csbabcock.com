d8fd7b8d3a7044d57f69a70859eda76d125368c5
csbabcock <52185272+csbabcock@users.noreply.github.com>
2021-05-20 11:04:04 -0400

    Updates

---
+++ precache-manifest.248e66adf7227ba934be6ba2e6260148.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf6483adb0d324525f667c1b4b104aca",
+    "url": "/csbabcock.com/index.html"
+  },
+  {
+    "revision": "74a202034f13f4378a4b",
+    "url": "/csbabcock.com/static/css/main.bdae5be4.chunk.css"
+  },
+  {
+    "revision": "cbee491d976ebb446f17",
+    "url": "/csbabcock.com/static/js/2.bd126fc0.chunk.js"
+  },
+  {
+    "revision": "74a202034f13f4378a4b",
+    "url": "/csbabcock.com/static/js/main.512809c4.chunk.js"
+  },
+  {
+    "revision": "f494f00521842379aac5",
+    "url": "/csbabcock.com/static/js/runtime-main.91dd5049.js"
+  }
+]);
